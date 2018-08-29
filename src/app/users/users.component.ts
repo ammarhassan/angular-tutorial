3a592eade046e3b37dd1f2f4279baef44f182d98
ammarhassan <mammar0009@gmail.com>
2018-08-29 00:01:08 -0400

    Adding user details and posts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import {DataService} from '../data.service';
 import {Observable} from 'rxjs';
 
+import {ActivatedRoute} from '@angular/router';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -12,7 +14,7 @@ export class UsersComponent implements OnInit {
 
   users$: Object;
 
-  constructor(private data: DataService) { }
+  constructor(private data: DataService) {}
 
   ngOnInit() {
     this.data.getUsers().subscribe(
